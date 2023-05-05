a6fa518a4d44e0dca4434fd917493196a2228f49
yanJuicy <hanjub3@gmail.com>
2023-05-05 15:39:52 +0900

    Todo List

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooterVue from "./components/TodoFooter.vue";
+import TodoHeaderVue from "./components/TodoHeader.vue";
+import TodoInputVue from "./components/TodoInput.vue";
+import TodoListVue from "./components/TodoList.vue";
+
+export default {
+  components: {
+    TodoHeader: TodoHeaderVue,
+    TodoInput: TodoInputVue,
+    TodoList: TodoListVue,
+    TodoFooter: TodoFooterVue
+  }
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f8;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
