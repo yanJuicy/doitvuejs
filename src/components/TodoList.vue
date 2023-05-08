b7a89cd9328ac4bb86afe892ee63933b6a7bdcb1
yanJuicy <hanjub3@gmail.com>
2023-05-08 21:26:17 +0900

    기존 애플리케이션 구조의 문제점 해결

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <ul>
-      <li v-for="todoItem in todoItems" :key="todoItem" class="shadow">
+      <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
         <i class="checkBtn fa fa-check" aria-hidden="true"></i>
         {{ todoItem }}
         <span
@@ -18,22 +18,10 @@
 
 <script>
 export default {
-  data() {
-    return {
-      todoItems: []
-    };
-  },
-  created() {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        this.todoItems.push(localStorage.key(i));
-      }
-    }
-  },
+  props: ["propsdata"],
   methods: {
     removeTodo(todoItem, index) {
-      localStorage.removeItem(todoItem);
-      this.todoItems.splice(index, 1);
+      this.$emit("removeTodo", todoItem, index);
     }
   }
 };
